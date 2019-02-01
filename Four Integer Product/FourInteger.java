5070bf116d46f20e8599e0b70a2b2f6e003df73f
Davindra O'Neal <davindraoneal@gmail.com>
2019-02-01 13:08:53 -0500

    Update FourInteger.java

--- Four Integer Product/FourInteger.java
+++ Four Integer Product/FourInteger.java
@@ -5,15 +5,15 @@ package fourinteger;
  * @author Davindra O'Neal
  * Purpose of Program Graded Exercise - To Calculate The Product of Four Integers
  */
-import java.util.Scanner;
+import java.util.Scanner; //This imports the Scanner utility which allows us to accept inputs
 public class FourInteger {
     //This is a Graded Exercise
     //This program finds the product of four integers and displays the result
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);// creates a Scanner object and assigns its reference to the variable inpu
         
         System.out.println("Enter the first number");
-        int firstnumber = input.nextInt();
+        int firstnumber = input.nextInt();// this declaration makes whatever the user enters is saved as the variable first number
         System.out.println("The number you've typed is " + firstnumber );
         System.out.println("Enter the second number");
         int secondnumber = input.nextInt();
